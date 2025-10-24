00025df5e4c63f3272b0e55e86362fcb8556d4cc
dionysm <dionysm@gmail.com>
2025-10-24 09:35:48 +0200

    Deepseek 0.2 -- Working Route  yaaay

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,23 +2,18 @@
 
 namespace Dio\OffertaOfferta\Controller;
 
-use Shopware\Core\Framework\Routing\Annotation\RouteScope;
-use Shopware\Core\Framework\Routing\Annotation\Since;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @RouteScope(scopes={"api"})
- */
-class ApiTestController extends AbstractController
+class TestController extends AbstractController
 {
-    /**
-     * @Since("6.7.3.1")
-     * @Route("/api/offerta/test", name="api.offerta.test", methods={"GET"}, defaults={"auth_required"=false})
-     */
-    public function test(): JsonResponse
+    public function apiTest(): JsonResponse
     {
         return new JsonResponse(['message' => 'API test route working!']);
+    }
+
+    public function storefrontTest(): JsonResponse
+    {
+        return new JsonResponse(['message' => 'Storefront test route working!']);
     }
 }
